2b3328d96abff2e673820ec460b7d4497cc555b0
Dimpurr <dimpurr@live.com>
2017-12-17 14:00:14 +0800

    fin layout

---
+++ src/hello.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <h3>Components</h3>
+    <p>{{ vue_hello }} MUGI</p>
+    <test></test>
+
+    <h3>Router</h3>
+    <p>
+        <router-link to="/test">Go to Test</router-link>
+    </p>
+    <router-view></router-view>
+
+</div>
+</template>
+
+<script>
+import test from './test.vue'
+
+module.exports = {
+    data: function() {
+        return {
+            vue_hello: "hello"
+        }
+    },
+    components: {
+        test: test
+    }
+}
+</script>
+
+<style scoped>
+p {
+    color: #777;
+}
+</style>
